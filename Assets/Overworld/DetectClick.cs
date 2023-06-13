c052a97f0c3924e023379e3ccc59b0a2382b7a58
OriSketchy <orisketchy@gmail.com>
2023-06-13 01:11:20 +0100

    Started tag detection script
    the plan is to trigger the dialogue script when player leftclicks on something with the "CanSpeak" tag
    It's currently not working
    try switching out raycaster for physics.raycast?

---
+++ Assets/Overworld/DetectClick.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DetectClick : MonoBehaviour, IPointerDownHandler
+{
+
+    //good luck
+    private void Start()
+    {
+        AddPhysicsRaycast();
+    }
+
+    private void AddPhysicsRaycaster()
+    {
+        PhysicsRaycaster physicsRaycaster = FindObjectOfType<PhysicsRaycaster>();
+        if (physicsRaycaster == null)
+        {
+            Camera.main.gameObject.AddComponent<PhysicsRaycaster>();
+        }
+        
+        if (physicsRaycaster.CompareTag("CanSpeak"))
+        {
+            Debug.Log("Youre did it");
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Clicked: " + eventData.pointerCurrentRaycast.gameObject.name);
+    }
+
+    void FixedUpdate()
+    {
+        if (PhysicsRaycaster.CompareTag("CanSpeak") != null)
+        {
+            Debug.Log(hit.transform.gameObject.name);
+        }
+    }
+}
--- Assets/Overworld/Dialogue.cs
+++ Assets/Overworld/Dialogue.cs
@@ -1,78 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using TMPro;
+
 
 public class Dialogue : MonoBehaviour
 {
-    public TextMeshProUGUI textComponent;
-    public string[] lines;
-    [Range(0f, 1f)] public float textSpeed;  //the lower the textSpeed the faster text moves
-    [Range(0f, 2f)] public float mouseCooldown;
+     public TextMeshProUGUI textComponent;
+     public string[] lines;
+     [Range(0f, 1f)] public float textSpeed;  //the lower the textSpeed the faster text moves
+     [Range(0f, 2f)] public float mouseCooldown;
 
-    private int index;
+     private int index;
+
+    [SerializeField]
+    LayerMask CanSpeak = 0;
 
     void Start()
     {
-        textComponent.text = string.Empty;
+         textComponent.text = string.Empty;
     }
-
-    void OnMouseDown()
+   
+    void Awake()
     {
-        // Debug.Log("Player has clicked " + gameObject.name);
-        StartDialogue();
-        // if(gameObject.CompareTag("CanSpeak"))
-        // {
-        //     StartDialogue();
-        // }
-        // else
-        // {
-        //     gameObject.SetActive(false);
-        // }
-    }
-
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
+        if(gameObject.CompareTag("CanSpeak"))
         {
-            if (textComponent.text == lines[index])
-            {
-                NextLine();
-            }
-            else
-            {
-                StopAllCoroutines();
-                textComponent.text = lines[index];
-            }
-        }
-    }
-
-    void StartDialogue()
-    {
-        index = 0;
-        StartCoroutine(TypeLine());
-    }
-
-    IEnumerator TypeLine()
-    {
-        foreach (char c in lines[index].ToCharArray())
-        {
-            textComponent.text += c;
-            yield return new WaitForSeconds(textSpeed);
-        }
-    }
-
-    void NextLine()
-    {
-        if(index < lines.Length - 1)
-        {
-            index++;
-            textComponent.text = string.Empty;
-            StartCoroutine(TypeLine());
+            StartDialogue();
         }
         else
         {
             gameObject.SetActive(false);
         }
     }
-}
+
+    void Update()
+     {
+         if(Input.GetMouseButtonDown(0))
+         {
+             if (textComponent.text == lines[index])
+             {
+                 NextLine();
+             }
+             else
+             {
+                 StopAllCoroutines();
+                 textComponent.text = lines[index];
+             }
+         }
+     }
+
+     void StartDialogue()
+     {
+         index = 0;
+         StartCoroutine(TypeLine());
+     }
+
+     IEnumerator TypeLine()
+     {
+         foreach (char c in lines[index].ToCharArray())
+         {
+             textComponent.text += c;
+             yield return new WaitForSeconds(textSpeed);
+         }
+     }
+
+     void NextLine()
+     {
+         if(index < lines.Length - 1)
+         {
+             index++;
+             textComponent.text = string.Empty;
+             StartCoroutine(TypeLine());
+         }
+         else
+         {
+             gameObject.SetActive(false);
+         }
+     }
+ }
