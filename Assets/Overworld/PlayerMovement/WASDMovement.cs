da9d0c3b2fc9d22a82b0762417076d6a587ef7ef
OriSketchy <orisketchy@gmail.com>
2023-06-17 01:49:12 +0100

    Updated movement (temporary fix)
    Movement is now stopped if any conflicting keys are pressed
    also fixed up duffins animator

--- Assets/Overworld/PlayerMovement/WASDMovement.cs
+++ Assets/Overworld/PlayerMovement/WASDMovement.cs
@@ -24,7 +24,7 @@ public class WASDMovement : MonoBehaviour
             direction += Vector3.right;
         }
         if (Input.GetKey("a"))
-        {
+        {   
             direction += Vector3.left;
         }
         if (Input.GetKey("w"))
@@ -35,17 +35,26 @@ public class WASDMovement : MonoBehaviour
         {
             direction += Vector3.back;
         }
+
+        //disable movement if more than 2 keys pressed
+        //there is probably a better way to do it, i'll hunt through the unity scripting API later
+        if(Input.GetKey("w") && Input.GetKey("s") && Input.GetKey("d")  ||  Input.GetKey("w")  && Input.GetKey("s") && Input.GetKey("a")  ||  Input.GetKey("w")  && Input.GetKey("d") && Input.GetKey("a")  ||  Input.GetKey("d")  && Input.GetKey("s") && Input.GetKey("a"))
+        {
+            direction = Vector3.zero;
+        }
         
         //Execute player input
         transform.position += direction.normalized * speed * Time.fixedDeltaTime; 
         
-        
+        //if getting any WASD input play walk animation
         if (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
         {
             animator.SetBool("Walk", true);
         }
 
-        if(!Input.GetKey("w") && !Input.GetKey("a") && !Input.GetKey("s") && !Input.GetKey("d"))
+        //if either not getting any  WASD input, or getting conflicted input play idle anim.
+        //There is definitely a better way to do this, possibly getting the velocity of the player and if they're above a certain speed enable walk anim?
+        if(!Input.GetKey("w") && !Input.GetKey("a") && !Input.GetKey("s") && !Input.GetKey("d")    ||     Input.GetKey("w") && Input.GetKey("s") || Input.GetKey("a")  && Input.GetKey("d")    ||     Input.GetKey("w") && Input.GetKey("s") && Input.GetKey("d") || Input.GetKey("w")  && Input.GetKey("s") && Input.GetKey("a")    ||    Input.GetKey("w")  && Input.GetKey("d") && Input.GetKey("a")  ||  Input.GetKey("d")  && Input.GetKey("s") && Input.GetKey("a"))
         {
             animator.SetBool("Walk", false);
         }
