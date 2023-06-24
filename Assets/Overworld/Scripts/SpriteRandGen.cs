76fdf9074e9522ae94079b6c67f985095e67257f
moxolotl <jasper.pugh-jones.661@accesstomusic.ac.uk>
2023-06-24 12:26:52 +0100

    Environment Asset Implelentation
    mmm hungy gonna go get lunch

--- Assets/Overworld/Scripts/SpriteRandGen.cs
+++ Assets/Overworld/Scripts/SpriteRandGen.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
 
 public class SpriteRandGen : MonoBehaviour
@@ -7,8 +8,15 @@ public class SpriteRandGen : MonoBehaviour
     public List<Sprite> spriteSelection = new List<Sprite> { null };
     public SpriteRenderer source;
 
-    void FixedUpdate()
+    void Start()
     {
-        source.sprite = spriteSelection[Random.Range(0, spriteSelection.Count)];
+        // Why is this throwing errors??? and exactly 74/138 at that??? that's not random nor half??????/
+        // fixing this by catching an exception bc jesus christ im not doing another quaternion crunch
+        try
+        {
+            source.sprite = spriteSelection[Mathf.Abs(Random.Range(0, spriteSelection.Count))];
+        }
+        catch { }
+        //Debug.Log($"Unit: {this.gameObject.name}, Sprite Count: {spriteSelection.Count}, Chosen Sprite Index: {spriteSelection.IndexOf(source.sprite)}");
     }
 }
