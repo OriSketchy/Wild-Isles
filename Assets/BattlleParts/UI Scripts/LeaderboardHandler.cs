f8f1451c6769e62586dfe6d59715b83a4a246f96
moxolotl <jasper.pugh-jones.661@accesstomusic.ac.uk>
2023-06-27 15:36:24 +0100

    Leaderboard implementation

--- Assets/BattlleParts/UI Scripts/LeaderboardHandler.cs
+++ Assets/BattlleParts/UI Scripts/LeaderboardHandler.cs
@@ -1,18 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using TMPro;
 
 public class LeaderboardHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    string text = string.Empty;
+    public TextMeshProUGUI leaderBox;
+
+    public string textDir;
+    public string directory;
+
+    public StreamReader reader;
+
+    private void Start()
     {
-        
+       // reader = new StreamReader(directory + textDir, true);
     }
 
-    // Update is called once per frame
-    void Update()
+    // To organise list:
+    // - split each line into an array (words 0, score 1)
+    // - put each array into a 2D array (words [x][0], score [x][1])
+    // - sort 2D array by score
+    // - write array back to file, combining each column into 1 line
+    public void SortScores()
     {
-        
+        reader = new StreamReader(directory + textDir);
+        // Use this to read and extract file
+        //string input = reader.ReadToEnd();
+        string[][] sortingArray = new string[0][];
+        sortingArray[0][0] = "DEBUG";
+        sortingArray[0][1] = "10000";
+
+        // Compare each line to neighbours. When position is found, move all items from bottom of the array down one and fill in blank with the line
+
+        // runs though each array element. Gets sorted position.
+        for (int i = 0; i <= sortingArray.Length; i++)
+        {
+            // split name/score (index 0, 1)
+            string[] newLine = reader.ReadLine().Split(" ");
+            int chosenIndex;
+
+            if (int.Parse(newLine[1]) <= int.Parse(sortingArray[i][1]))
+            {
+                chosenIndex = i;
+                for (int a = sortingArray.Length; a >= chosenIndex; a--)
+                {
+                    Debug.LogError(sortingArray[a][0] + " " + sortingArray[a][1]);
+                    sortingArray[a + 1] = sortingArray[a];
+                }
+                sortingArray[i] = newLine;
+            }        
+        }
+
+        // Use this to rewrite file
+        Debug.Log(sortingArray.Length);
+        reader.Close();
+        using (StreamWriter writer = new StreamWriter(directory + textDir))
+        {
+            string output = string.Empty;
+            string outputL = string.Empty;
+            //for (int i = 0; i < sortingArray.Length; i++)
+            //{
+            //    output = $"{sortingArray[i][0]}:{sortingArray[i][1]}";
+            //    if(i<=10)
+            //        outputL += $"{output}\n";
+            //}
+
+            // TEMP (this copies the loop)
+            int b = 1;
+            while (b < sortingArray.Length)
+            {
+                output = $"{sortingArray[b][0]}:{sortingArray[b][1]}";
+                if (b <= 10)
+                    outputL += $"{output}\n";
+                b++;
+            }
+            leaderBox.text = outputL;
+            writer.WriteLine(output);
+            writer.Close();
+        }
+    }
+    public void ReadScores()
+    {
+        reader = new StreamReader(directory + textDir);
+        string outputL = string.Empty;
+
+        int b = 0;
+        while(b<10)
+        {
+            Debug.Log("Loop");
+
+            string[] a = reader.ReadLine().Split(" ");
+            outputL += $"{a[0]} : {a[1]}\n";
+
+            Debug.Log(outputL);
+            b++;
+        }
+        leaderBox.text = outputL;
     }
 }
--- Assets/BattlleParts/UI Scripts/MainMenu.cs
+++ Assets/BattlleParts/UI Scripts/MainMenu.cs
@@ -2,38 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// List of menu states
+public enum MenuState { MAIN, PLAY, OPTIONS, CREDITS }
+
 public class MainMenu : MonoBehaviour
 {
     public GameObject keyAssetParent;
     public GameObject playParent;
     public GameObject optionsParent;
     public GameObject creditsParent;
+
+    private MenuState state;
     public void OnExitButton()
     {
         // Exit game
         Application.Quit();
+        Debug.Log("Exitted game");
     }
     public void OnPlayButton()
     {
         // Go to input then play screen (WRITE TO FILE)
         keyAssetParent.SetActive(false);
         playParent.SetActive(true);
+        state = MenuState.PLAY;
     }
     public void OnOptionsButton()
     {
         // Go to options screen
         keyAssetParent.SetActive(false);
         optionsParent.SetActive(true);
+        state = MenuState.OPTIONS;
     }
     public void OnCreditsButton()
     {
         // Go to credits screen
         keyAssetParent.SetActive(false);
         creditsParent.SetActive(true);
+        state = MenuState.CREDITS;
     }
     public void OnBackButton()
     {
         // Go to main screen
-
+        switch (state)
+        {
+            case MenuState.CREDITS:
+                creditsParent.SetActive(false);
+                break;
+            case MenuState.OPTIONS:
+                optionsParent.SetActive(false);
+                break;
+            case MenuState.PLAY:
+                playParent.SetActive(false);
+                break;
+        }
+        keyAssetParent.SetActive(true);
+        state = MenuState.MAIN;
     }
 }
